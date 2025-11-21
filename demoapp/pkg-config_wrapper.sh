11f1845e672fc7f2af8c4d823191ead230603b5a
Dickens1308 <83779911+Dickens1308@users.noreply.github.com>
2025-11-21 09:55:19 +0900

    Updating card config file

--- demoapp/pkg-config_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PKG_CONFIG_SYSROOT_DIR=/home/dart/aep/sdk-4.18/toolchain-cortexa9hf-2.4/sysroots/cortexa9hf-neon-poky-linux-gnueabi
-export PKG_CONFIG_SYSROOT_DIR
-PKG_CONFIG_LIBDIR=/home/dart/aep/sdk-4.18/toolchain-cortexa9hf-2.4/sysroots/cortexa9hf-neon-poky-linux-gnueabi/usr/lib/pkgconfig:/home/dart/aep/sdk-4.18/toolchain-cortexa9hf-2.4/sysroots/cortexa9hf-neon-poky-linux-gnueabi/usr/share/pkgconfig:/home/dart/aep/sdk-4.18/toolchain-cortexa9hf-2.4/sysroots/cortexa9hf-neon-poky-linux-gnueabi/aep-dev/lib/pkgconfig
-export PKG_CONFIG_LIBDIR
-exec pkg-config "$@"
